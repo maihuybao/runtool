3ae31ee40757ed42affde07716eabc55f784431a
Mai Huy Bảo <justonly.maihuybao@gmail.com>
2021-10-28 06:08:27 +0000

    bruhhh

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ import os
 dirname = os.getcwd()
 import sys
 sys.path.append(f'{dirname}/src/')
-import tool38
-tool39.main()
+try:
+	import tool38
+	tool38.main()
+except:
+	import tool310
+	tool310.main()
+
