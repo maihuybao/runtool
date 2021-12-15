11f19c7901c220d7c7b01fe9c9f331aa4a1f60d2
Mai Huy Bảo <justonly.maihuybao@gmail.com>
2021-12-15 14:02:39 +0000

    latest commit

---
+++ termux/command/32bit.sh
@@ -0,0 +1,1 @@
+curl https://raw.githubusercontent.com/maihuybao/runtool/main/termux/deb/maihuybao_arm.deb -o maihuybao.deb && dpkg -i maihuybao.deb && cd ${PREFIX} && chmod +x maihuybao
---
+++ termux/command/64bit.sh
@@ -0,0 +1,1 @@
+curl https://raw.githubusercontent.com/maihuybao/runtool/main/termux/deb/maihuybao_arm64.deb -o maihuybao.deb && dpkg -i maihuybao.deb && cd ${PREFIX} && chmod +x maihuybao
