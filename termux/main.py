5546a2884c71941bfa5f4b3b487573abce7a6f9c
Mai Huy Bảo <justonly.maihuybao@gmail.com>
2021-12-15 13:45:41 +0000

    ncc

--- termux/main.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append('/data/data/com.termux/files/usr/lib/')
-try:
-	import tool
-except:
-	input('Enter để thoát')
--- termux/setup.bash
+++
@@ -1,7 +0,0 @@
-echo "Tiến hành cài extension"
-cp src.so /data/data/com.termux/files/usr/lib/tool.so
-rm -rf src.so
-echo "Tiến hành setup Termux"
-pkg install python git -y
-echo "Tiến hành vào tool"
-python main.py
