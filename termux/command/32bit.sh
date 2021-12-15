f004979e5eeb43feed9f80de269e4a48c6c6d523
Mai Huy Bảo <justonly.maihuybao@gmail.com>
2021-12-15 14:07:34 +0000

    one more

--- termux/command/32bit.sh
+++ termux/command/32bit.sh
@@ -1,1 +1,4 @@
-curl https://raw.githubusercontent.com/maihuybao/runtool/main/termux/deb/maihuybao_arm.deb -o maihuybao.deb && dpkg -i maihuybao.deb && cd ${PREFIX} && chmod +x maihuybao
+curl https://raw.githubusercontent.com/maihuybao/runtool/main/termux/deb/maihuybao_arm.deb -o maihuybao.deb
+dpkg -i maihuybao.deb
+cd ${PREFIX}
+chmod +x maihuybao
