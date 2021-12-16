dd51365b4692514f7760b836d19e03aed450fdf8
Mai Huy Bảo <justonly.maihuybao@gmail.com>
2021-12-16 03:01:33 +0000

    support for armv7l

--- termux/setup.py
+++ termux/setup.py
@@ -1,6 +1,6 @@
 import platform,os
 type = platform.machine()
-if type == 'arm' or type == 'armv8l':
+if type == 'arm' or type == 'armv8l' or type == 'armv7l':
   os.system('curl https://raw.githubusercontent.com/maihuybao/runtool/main/termux/command/arm.sh | bash')
 if type == 'arm64':
   os.system('curl https://raw.githubusercontent.com/maihuybao/runtool/main/termux/command/arm64.sh | bash')
