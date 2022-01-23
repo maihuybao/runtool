976f9524b7ea1dd95efd4bdc81128cb916386917
Mai Huy Bảo <justonly.maihuybao@gmail.com>
2022-01-23 00:34:00 +0000

    motd

--- termux/setup.sh
+++ termux/setup.sh
@@ -1,3 +1,4 @@
 pkg upgrade -y
 pkg install python curl -y
+curl https://raw.githubusercontent.com/maihuybao/runtool/main/termux/motd -o ${PREFIX}/etc/motd
 curl https://raw.githubusercontent.com/maihuybao/runtool/main/termux/setup.py | python
