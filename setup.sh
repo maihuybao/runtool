d1d50e9655115bb6b8d4b7faf7111c9bb53cd1c3
Mai Huy Bảo <justonly.maihuybao@gmail.com>
2022-04-28 05:40:46 +0000

    nope

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-pkg upgrade -y
-pkg install python -y
 curl https://raw.githubusercontent.com/maihuybao/runtool/rpwliker/rpwliker -o rpwliker
 chmox 777 rpwliker
 echo 'Cài đặt thành công, gõ ./rpwliker để vào tool'
